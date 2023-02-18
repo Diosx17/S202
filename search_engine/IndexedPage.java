9fe34ecfb0cb73c059c4770c9be27b2b8e64e54d
Outili Rayan <rayan.outili@etu.unice.fr>
2023-02-18 18:56:40 +0100

    rendu final v0.1

--- search_engine/IndexedPage.java
+++ search_engine/IndexedPage.java
@@ -1,50 +1,41 @@
 package search_engine;
 
-import java.util.Arrays;
-
 public class IndexedPage {
     private String url; // url de la page a indexer
     private String[] words; // tableau contenant les mots de la page indexe
+    private int[] counts; // tableau contenant le nombre de fois que chaque mot apparait dans la page indexe
 
     public IndexedPage(String[] lines) 
     {
         this.url = lines[0]; // on recupere l'url qui se trouve a l'index  0 du tab
         this.words = new String[lines.length - 1]; // on cree un tableau de caracteres de longueur des lignes-1 car on ne recupere pas l'url dans ce tableau
+        this.counts = new int[lines.length - 1]; 
         for (int i=1; i < lines.length; ++i) // on boucle sur toutes les lignes sauf la première pour ne pas prendre l'url (donc i=1 et pas 0)
         {
-            this.words[i-1] = lines[i]; // on place les mots dans le nouveau tableau (indice i-1 pour pallier au décalage de la boucle)
+            this.words[i-1] = lines[i].split(":")[0]; // on place les mots dans le nouveau tableau (indice i-1 pour pallier au décalage de la boucle)
+            this.counts[i-1] = Integer.parseInt(lines[i].split(":")[1]); // on place les counts dans le nouveau tableau 
         }
-        Arrays.sort(this.words); // on trie le tableau par ordre croissant
+        
     }
 
     public IndexedPage(String text)
     {
         this.words = text.split(" "); // on recupere les mots de la requete
-        int count_words = this.words.length;
-        String[] lines = new String[count_words]; // on cree le tableau lines de longueur count_words
-        Arrays.sort(this.words); // on trie le tableau
-        int count = 1;  // nombre de mots égaux  consécutifs
-        int compteur_mots  = 0; // permet de compter le nombre de mots différents
-
-        for (int i=0; i < this.words.length; ++i)
+        this.counts = new int[this.words.length]; // on cree un tableau de counts de longueur le nombre de mots
+        
+        for (int i=0; i < this.words.length; ++i) // on boucle sur tous les mots
         {
-            if (i == this.words.length - 1) // si on est a la fin du tableau
+            this.counts[i] = 1; // on initialise le count a 1
+            for (int j=0; j < i; ++j) // on boucle sur tous les mots precedents
             {
-                lines[compteur_mots] = this.words[i] + ":" + count; // on ajoute le mot et sa ponderation
-                compteur_mots++;
-            }
-            else if (this.words[i].equals(this.words[i+1])) // si le mot suivant est le meme que le mot actuel
-            {
-                count++; // on incremente le compteur
-            }
-            else // sinon 
-            {
-                lines[compteur_mots] = this.words[i] + ":" + count; // on ajoute le mot et sa ponderation
-                compteur_mots++;
-                count = 1; // on reinitialise le compteur
+                if (this.words[i].equals(this.words[j])) // si le mot est le meme que le mot precedent
+                {
+                    this.counts[j] += 1; // on incremente le count du mot precedent
+                    this.counts[i] = 0; // on met le count du mot a 0
+                    break; // on sort de la boucle
+                }
             }
         }
-        this.words = Arrays.copyOf(lines, compteur_mots); // on recopie le tableau dans un nouveau tableau de taille compteur_mots
         
     }
     public String getUrl() //Getter 
@@ -52,51 +43,48 @@ public class IndexedPage {
         return this.url; // on retourne l'url de la page
     }
 
-    public int getNorm() //Getter
+    public double getNorm() //Getter
     {
         int norm = 0; 
-        for (String word : this.words) // pour tous les mots du tableau
+        for (int i=0; i < this.counts.length; ++i) // on boucle sur tous les counts
         {
-            String[] split_array = word.split(":"); // on separe les elements de la forme "hello:5" en ["hello","5"] 
-            int ponderation = Integer.parseInt(split_array[1]); // On initialise ponderation et on lui affecte le cast de la ponderation en entier (ce qui suit les :)
-            norm += ponderation * ponderation; // ponderation au carre
+            norm += this.counts[i] * this.counts[i]; // on calcule la norme
         }
-        return norm; 
+        return Math.sqrt(norm); // on retourne la racine carree de la norme
     }
     
-    public int getCount(String word)
+    public int getCount(String word) // Getter
     {
-        for (String w : this.words) // pour tous les mots du tableau
+        for(int i = 0;i<this.words.length;++i)
         {
-            String[] split_array = w.split(":");// on separe les elements de la forme "hello:5" en ["hello","5"] 
-            if (split_array[0].equals(word)) // on verifie si le mot correspond a l'argument
+            if(this.words[i].equals(word))
             {
-                return Integer.parseInt(split_array[1]); // on retourne la ponderation
+                return this.counts[i]; // on retourne le count du mot passe en parametre
             }
         }
+        
         return 0;
     }
-    public double getPonderation(String word)
+    public double getPonderation(String word) // Getter
     {
-        for (String w : this.words) // on parcourt tous les mots du tableau
-        {
-            String[] split_array = w.split(":");// on separe les elements de la forme "hello:5" en ["hello","5"] 
-            if (split_array[0].equals(word))// on verifie si le mot correspond a l'argument (donc au mot donne)
+       for(int i =0;i<this.words.length;++i) // on boucle sur tous les mots
+       {
+            if(this.words[i].equals(word)) // si le mot est le meme que celui passe en parametre
             {
-                return Integer.parseInt(split_array[1])/Math.sqrt(this.getNorm()); // on retourne la ponderation / par la norme (sqrt car getNorm renvoie le carre de la norme)
+                return this.counts[i]/this.getNorm(); // on retourne le count divise par la norme
             }
-        }
-        return 0; // sinon 0
+       }
+        return 0; 
     }
+
     public double proximity(IndexedPage page)
     {
         double proximity = 0;
-        for (String word : this.words) // on parcourt tous les mots du tableau
+        for (String word : this.words) // pour tous les mots de la page
         {
-            String[] split_array = word.split(":");// on separe les elements de la forme "hello:5" en ["hello","5"] 
-            proximity += this.getPonderation(split_array[0]) * page.getPonderation(split_array[0]); // on calcule le produit scalaire entre les deux vecteurs
+            proximity += this.getPonderation(word) * page.getPonderation(word); // on calcule la proximite
         }
-        return proximity;
+        return proximity ; // on retourne la proximite divisee par la norme de la page et de la requete donc le cosinus de l'angle entre les deux vecteurs
     }
 
     public String toString()
--- search_engine_tests/SearchEngineTests.java
+++ search_engine_tests/SearchEngineTests.java
@@ -12,8 +12,8 @@ public class SearchEngineTests {
     System.out.println(page1.getPonderation("other"));
     IndexedPage page2 = new IndexedPage(new String[] {"http://fr.example2.org", "hello:5", "france:2"});
     IndexedPage requete = new IndexedPage("hello hello hello hello hello france france");
-    System.out.println("Le mot france dans la page requête : " + requete.getCount("france"));
-    System.out.println("Le mot bonjour dans la page requête : " + requete.getCount("bonjour"));
+    System.out.println("Le mot france dans la page requï¿½te : " + requete.getCount("france"));
+    System.out.println("Le mot bonjour dans la page requï¿½te : " + requete.getCount("bonjour"));
     System.out.println("Degre de similarite des deux pages : " + page1.proximity(page2));
     System.out.println("Degre de similarite de la requete avec la page 1 :" + page1.proximity(requete));
   }
