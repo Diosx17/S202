cb8b2947c8da3ce52575fe5338336e2111e82241
Diosx17 <bmelon06800@gmail.com>
2023-02-26 15:12:46 +0100

    coverage vite fait

--- SearchEngineTests.java
+++ SearchEngineTests.java
@@ -8,8 +8,6 @@ public class SearchEngineTests
 {
   public static void main(String[] args) throws Exception
   {
-    
-
     URL location = SearchEngine.class.getProtectionDomain().getCodeSource().getLocation();
     Path binFolder = Paths.get(location.toURI());
     Path indexFolder = binFolder.resolve("INDEX");
@@ -20,6 +18,20 @@ public class SearchEngineTests
     // On crée une requête.
     String requestString = "cerise flan";
     IndexedPage request = new IndexedPage(requestString);
+    request.getCount("cerise");
+    request.getCount("flan");
     se.printResults(requestString);
+
+    IndexedPage request2 = new IndexedPage(new String[] {"https://vikidia.org/wiki/Flan cerise:1 flan:1"});
+    request2.getCount("cerise");
+    request2.getCount("flan");
+    System.out.println(request2);
+    
+    SearchResult[] tableau_resultats = se.launchRequest(requestString);
+    for (SearchResult result : tableau_resultats)
+    {
+      System.out.println(result.getUrl()+" : " + result.getScore());
+    }
+  
   }
 }
