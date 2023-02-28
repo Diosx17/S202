01d43e0709dcbe372a618d8b186de2b5cf44eb95
portelette thomas <thomas.portelette@etu.unice.fr>
2023-02-28 16:40:23 +0100

    Branche a jour v03

--- search_engine/SearchEngine.java
+++ search_engine/SearchEngine.java
@@ -1,6 +1,9 @@
 package search_engine;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.io.File;
 import java.util.Map;
 import java.util.HashMap;
@@ -9,7 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Comparator;
 import java.util.Scanner;
-import java.nio.file.Paths;
+import java.util.Arrays;
+
 
 public class SearchEngine 
 {
@@ -144,6 +148,100 @@ public class SearchEngine
 		
 		
 	}
+	//LectureFichier
+    public static String lire(String nomFichier) throws Exception {
+        String contenu = "";
+        File fichier = new File(nomFichier);
+        Scanner lecteur = new Scanner(fichier);
+        while (lecteur.hasNextLine()) {
+            contenu += lecteur.nextLine() + " ";
+        }
+        lecteur.close();
+
+        return contenu;
+    }
+
+    //majuscule en minuscule
+    public static String minuscule(String chaine) {
+        String chaineMinuscule = "";
+        for (int i = 0; i < chaine.length(); i++) {
+            //si le caractère est une majuscule on le transforme en minuscule
+            chaineMinuscule += Character.toLowerCase(chaine.charAt(i));
+        }
+        return chaineMinuscule;
+    }
+
+    //Fonction pour enlever tous les caractères n'étant pas des lettres
+    public static String enleverCaracteresSpeciaux(String chaine) {
+        String chaineSansCaracteresSpeciaux = "";
+        for (int i = 0; i < chaine.length(); i++) {
+            //si le caractère est une apostrophe ou des guillemets, on le remplace par un espace
+            if (chaine.charAt(i) == '\'' || chaine.charAt(i) == '"' || chaine.charAt(i) == '’') {
+                chaineSansCaracteresSpeciaux += " ";
+            }
+            //si le caractère est une lettre ou un espace on l'ajoute a la chaine
+            else if (Character.isLetter(chaine.charAt(i)) || chaine.charAt(i) == ' ') {
+                chaineSansCaracteresSpeciaux += chaine.charAt(i);
+            }
+        }
+        return chaineSansCaracteresSpeciaux;
+    }
+
+    //Fonction permettant d'enlever les mots qui appartiennent à ListeNoire.txt
+    public static String enleverMotsListeNoire(String chaine) throws Exception {
+        String listeNoire = lire("./txt/ListeNoire.txt");
+        listeNoire = enleverCaracteresSpeciaux(listeNoire);
+        String[] listeNoireTableau = listeNoire.split(" ");
+        String[] chaineTableau = chaine.split(" ");
+        String chaineSansMotsListeNoire = "";
+        for (int i = 0; i < chaineTableau.length; i++) {
+            if (!Arrays.asList(listeNoireTableau).contains(chaineTableau[i])) {
+                chaineSansMotsListeNoire += chaineTableau[i] + " ";
+            }
+        }
+        return chaineSansMotsListeNoire;
+    }
+
+    //enlever mot de 1 et 2 lettres
+    public static String enleverMotsPetits(String chaine) {
+        String[] chaineTableau = chaine.split(" ");
+        String chaineSansMotsPetits = "";
+        for (int i = 0; i < chaineTableau.length; i++) {
+            if (chaineTableau[i].length() > 2) {
+                chaineSansMotsPetits += chaineTableau[i] + " ";
+            }
+        }
+        return chaineSansMotsPetits;
+    }
+
+    //lemmatise
+    public static String lemmatise(String chaine) throws Exception {
+        String dictPath = "S202/src/test03/dictiofr.txt";
+        // On crée la hashmap
+        Map<String, String> lemmaDict = new HashMap<>();
+        // On lit le fichier dictiofr.txt ligne par ligne
+        for (String line : Files.readAllLines(Paths.get(dictPath), StandardCharsets.UTF_8)) {
+            // On split chaque ligne en deux séparées les ":" puis on ajoute le mot et sa lemmatisation dans
+            // la hashmap
+            String[] dictSplit = line.split(":");
+            lemmaDict.put(dictSplit[0], dictSplit[1]);
+        }
+        String[] chaineTableau = chaine.split(" ");
+        String chaineLemmatisée = "";
+        for(int i = 0; i < chaineTableau.length; i++) {
+            // On regarde si le mot est dans la hashmap avec la fonction get
+            String lemma = lemmaDict.get(chaineTableau[i]);
+            // Si c'est le cas on ajoute le mot lemmatisé et un espace à la suite
+            if (lemma != null) {
+                chaineLemmatisée += lemma + " ";
+            }
+            // Sinon on ajoute le mot inchangé et un espace à la suite
+            else {
+                chaineLemmatisée += chaineTableau[i] + " ";
+            }
+        }
+        return chaineLemmatisée;
+    }
 
 	
 	
